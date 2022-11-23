2fc446e943a7ac6a48b9a2fc10c2f80604815040
Jeff Oliver <jcoliver@email.arizona.edu>
2022-11-23 10:32:28 -0700

    Add total counts for Radecki & Springer

--- compare-radecki-springer.R
+++ compare-radecki-springer.R
@@ -41,3 +41,29 @@ rs_summary <- data.frame(service = c("Geospatial", "Geospatial", "Statistical", 
                          modality = c("Consult", "Training", "Consult", "Training"),
                          counts = c(geo_consult, geo_training, stat_consult, stat_training))
 rs_summary$perc <- rs_summary$counts/nrow(rs)
+
+########################################
+# Also calculate the total  % of institutions with either consult/training for 
+# stats and geospatial for *all* institutions in R&S 2020
+rs <- read.csv(file = "data/Radecki-Springer-R1-raw.csv")
+
+# Count as supporting geo if Consulting or Training
+geo_rows <- unique(c(grep(pattern = "Geospatial",
+                          x = rs$Library..Consulting.), 
+                     grep(pattern = "Geospatial",
+                          x = rs$Library..Training.Events.)))
+rs$geo <- 0
+rs$geo[geo_rows] <- 1
+
+# Count as supporting stats if Consulting or Training
+stats_rows <- unique(c(grep(pattern = "Statistical",
+                            x = rs$Library..Consulting.),
+                       grep(pattern = "Statistical",
+                            x = rs$Library..Training.Events.)))
+rs$stats <- 0
+rs$stats[stats_rows] <- 1
+
+# Create summary data frame
+any_geo_stats <- data.frame(area = c("geo", "stats"),
+                            counts = c(sum(rs$geo), sum(rs$stats)))
+any_geo_stats$perc <-any_geo_stats$counts/nrow(rs)
